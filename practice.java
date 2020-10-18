40333b4c669839787f9bbd88b3c72a342fc5ef60
Forest Rees <fjrees4@outlook.com>
2020-10-18 15:28:27 -0700

    Test Code for Learning Software
    Hell Yeeaaaah

---
+++ practice.java
@@ -0,0 +1,6 @@
+public class practice{
+public static void main(String[] args) {}
+    limelight limelight=new limelight();
+    limelight.gettx();
+    printlimelight.gettx
+    print("Hello World");
